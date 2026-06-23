85723f9981957ccd610c2fbaaeb125588b998345
Mayank Yadav <mayankyadav805735@gmail.com>
2026-06-23 11:05:35 +0530

    changes

--- core-java-practice/gcr-codebase/programming-elements/L1/Age.java
+++
@@ -1,7 +0,0 @@
-class Age{
-	public static void main(String[] args){
-		int birth_year=2000;
-		int current_Year=2024;
-		System.out.print(" Harry's age in 2024 is"+ (current_Year-birth_year));
-	}
-}
